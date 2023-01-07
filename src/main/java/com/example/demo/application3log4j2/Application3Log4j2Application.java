26f213c78aef90e8fd30d3f64474367d47f6dcb2
tekuteku-tech <tekuteku.tech.mail@gmail.com>
2023-01-07 11:53:46 +0900

    init

---
+++ src/main/java/com/example/demo/application3log4j2/Application3Log4j2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo.application3log4j2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application3Log4j2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application3Log4j2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/application3log4j2/Application3Log4j2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.application3log4j2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application3Log4j2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
