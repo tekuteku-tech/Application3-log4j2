6c822867ceb4974e917b66c5d41ebdc353968984
tekuteku-tech <tekuteku.tech.mail@gmail.com>
2023-01-07 22:55:01 +0900

    フォーマットかけた

--- src/main/java/com/example/demo/application3log4j2/Application3Log4j2Application.java
+++ src/main/java/com/example/demo/application3log4j2/Application3Log4j2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application3Log4j2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application3Log4j2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application3Log4j2Application.class, args);
+    }
 
 }
